45e16defe66d0cc1272fcade445021c4e6b8f47f
Joshua Tamang <joshuatamang233@gmail.com>
2022-11-29 11:08:00 +0545

    merged with shiva branch

---
+++ parichaya_page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sushant
+ * Date: 5/9/2020
+ * Time: 4:08 PM
+ */
+include ('header.php') ?>
+
+    <div id="parichaya-page">
+
+    
+
+    </div>
+<?php include ('footer.php') ?>
